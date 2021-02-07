b57935576c4631586ac98621343edcad7ab06ba3
Edward Mitchell <edwardinchina@outlook.com>
2021-02-07 11:57:29 -0500

    food

--- src/pygame template.py
+++ src/pygame template.py
@@ -25,9 +25,16 @@ class Snake(object):
         self.tail = [p2]
         self.color = color
 
-    def update(self,v):
+    def update(self,v,food):
         self.tail.insert(0,self.head) #old head become being of tail
         self.head = move(self.head,v) #new head is old head + v
+        
+        
+        for rat in food:
+            if(self.head == rat):
+                food.remove(rat)
+                self.grow = True
+                print(len(self.tail))
         
         if(not self.grow):
             self.tail.pop() #removes last part of tail
@@ -40,6 +47,7 @@ class Snake(object):
         
         headBox = pygame.Rect(x, y, 10, 10)
         
+        
         pygame.draw.rect(screen, self.color, headBox)
         
         for t in self.tail:
@@ -50,7 +58,9 @@ class Snake(object):
 pygame.init() 
 clock = pygame.time.Clock()
 
-screen = pygame.display.set_mode((1600,1200)) 
+screen_width = 1600
+screen_height = 1200
+screen = pygame.display.set_mode((screen_width,screen_height)) 
 
 # Initialing Color 
 red = (255,0,0) 
@@ -60,7 +70,7 @@ yellow = (255,255,0)
 white = (255,255,255)
 black = (0,0,0)
 gray = (150,150,150)
-
+brown = (150,75,0)
 
 player = Snake((20,15),(21,15),red)
 
@@ -69,7 +79,12 @@ AI_Snakes = [Snake((30,15),(31,15),green),
              Snake((50,15),(51,15),yellow),
              Snake((60,15),(61,15),black),
              Snake((70,15),(71,15),white)]
-  
+
+food = []
+squirrels = []
+
+timer = 0
+
 while(True):
     screen.fill(gray)
         
@@ -78,23 +93,46 @@ while(True):
         break                   #   ... leave game loop
     
     
+    ## food
+    if timer == 0:
+        x = random.randint(0, screen_width/10)
+        y = random.randint(0, screen_height/10)
+        food.append((x,y))
+        
+        
+        x = random.randint(0, screen_width/10)
+        y = random.randint(0, screen_height/10)
+        squirrels.append((x,y))
+        
+    
+    for rat in food:
+        ratBox = pygame.Rect(*scale(rat,10), 10, 10)
+        pygame.draw.rect(screen, brown, ratBox)
+    
+    
+    for s in squirrels:
+        sBox = pygame.Rect(*scale(s,10), 10, 10)
+        pygame.draw.rect(screen, white, sBox)
+    
     v = random.choice([(-1, 0),(1, 0),(0, -1),(0, 1)])
     
     if len(player.tail) < 100:
         player.grow = True
-    player.update(v)
+    player.update(v,food)
     player.draw(screen)
 
     for ai in AI_Snakes:
         v = random.choice([(-1, 0),(1, 0),(0, -1),(0, 1)])
         if len(ai.tail) < 100:
             ai.grow = True
-        ai.update(v)
+        ai.update(v,food)
         ai.draw(screen)
     
     pygame.display.flip()
     
     
     clock.tick(50)
+    timer += 1
+    timer %= 3
 #end while
 pygame.quit()
